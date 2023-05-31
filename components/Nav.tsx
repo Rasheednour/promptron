a914b47c77fd9288b99956d2953a3e9120c4c673
Rasheednour <rasheednour@gmail.com>
2023-05-31 23:40:27 +0200

    FEAT: added logo and app name that links to home

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+type Props = {};
 
 const Nav = (props: Props) => {
+  const userLoggedIn = true;
   return (
-    <div>Nav</div>
-  )
-}
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          width={60}
+          height={60}
+          alt="PromptyDumpy logo"
+          className="object-contain"
+        />
+        <p className="logo_text">PromptyDumpty</p>
+      </Link>
 
-export default Nav
+    </nav>
+  );
+};
+
+export default Nav;
