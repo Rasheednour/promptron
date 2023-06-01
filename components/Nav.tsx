15148c577fbe44f2f75a1c5c4572c38b6705bbac
Rasheednour <rasheednour@gmail.com>
2023-06-02 01:49:40 +0200

    FEAT: signin/signout desktop navbar functionality

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,10 +5,58 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { list } from "postcss";
 type Props = {};
 
+// add provider type to be used in getProviders function
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  [key: string]: any;
+};
+
 const Nav = (props: Props) => {
-  const userLoggedIn = true;
+  // keep track of user login status
+  const userLoggedIn: boolean = true;
+
+  const [providers, setProviders] = useState<Record<string, Provider> | null>(
+    null
+  );
+
+  useEffect(() => {
+    const setProvider = async () => {
+      const response: Record<string, Provider> | null = await getProviders();
+      setProviders(response);
+    };
+    setProvider();
+  }, []);
+  // function to handle signOut event
+  const handleSignOut = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    try {
+      await signOut({ callbackUrl: "http://localhost:3000" });
+    } catch (error) {
+      console.error("Sign out failed", error);
+    }
+  };
+
+  // function to handle signIn even
+  const handleSignIn = async (
+    event: React.MouseEvent<HTMLButtonElement>,
+    providerName: string
+  ) => {
+    event.preventDefault();
+    try {
+      await signIn(providerName);
+    } catch (error) {
+      console.error("Sign in failed", error);
+    }
+  };
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -21,7 +69,44 @@ const Nav = (props: Props) => {
         />
         <p className="logo_text">PromptyDumpty</p>
       </Link>
-
+      <div className="sm:flex hidden">
+        {userLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button
+              type="button"
+              onClick={handleSignOut}
+              className="outline_btn"
+            >
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile picture"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={(e) => handleSignIn(e, provider.name)}
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
