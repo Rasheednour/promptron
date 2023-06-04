f4a6b115840099ee0d9dd2abe76c25483c96f804
Rasheednour <rasheednour@gmail.com>
2023-06-04 04:46:02 +0200

    FEAT: added form to create page

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+import { useState } from "react"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from '@components/Form';
 
 type Props = {}
 
 const CreatePrompt = (props: Props) => {
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+    const createPrompt = async (e:any) => {
+        return e;
+    }
   return (
-    <div>Create Prompt Page</div>
+    <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+    />
   )
 }
 
