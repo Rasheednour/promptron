3cc48ed6cdaaf941242c871df4023a88116da801
Rasheednour <rasheednour@gmail.com>
2023-06-04 20:07:25 +0200

    FEAT: search bar in home feed + prompts API fetch

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
+"use client";
+import { useState, useEffect} from 'react'
+import PromptCard from './PromptCard';
 
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((prompt) => (
+        <PromptCard
+        key={prompt.id}
+        prompt={prompt}/>
+      ))}
+    </div>
+  )
+}
 type Props = {}
 
 const Feed = (props: Props) => {
+  const [searchText, setSearchText] = useState('');
+  const [prompts, setPrompts] = useState([]);
+  const handleSearchChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPrompts(data);
+    }
+    fetchPosts();
+  }, [])
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input type='text' placeholder='Search for a tag or username'
+      value={searchText}
+      onChange={handleSearchChange}
+      required
+      className='search_input peer'/>
+        
+
+      </form>
+      <PromptCardList
+      data={prompts}
+      handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
