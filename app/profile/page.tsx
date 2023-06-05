3dd77fa52a8648b65f472eee359d71194baaf32d
Rasheednour <rasheednour@gmail.com>
2023-06-05 20:23:11 +0200

    FEAT: delete prompt works on the frontend/backend

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -25,6 +25,17 @@ const UserProfile = (props: Props) => {
       router.push(`/update-prompt?id=${post._id}`)
     }
     const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+      if(hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {method: 'DELETE'});
+          const filteredPosts = prompts.filter((p) => p._id !== post._id);
+          setPrompts(filteredPosts);
+        } catch (error) {
+          console.log(error);
+        }
+      }
 
     }
   return (
