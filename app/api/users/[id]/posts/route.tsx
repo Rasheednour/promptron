8bba7c6a44701403f4043d1e0f51495097581396
Rasheednour <rasheednour@gmail.com>
2023-06-10 01:10:13 +0200

    FIX: build errors due to typings

--- app/api/users/[id]/posts/route.tsx
+++ app/api/users/[id]/posts/route.tsx
@@ -1,8 +1,8 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
-import { NextApiRequest } from "next";
+import { NextRequest } from "next/server";
 
-export const GET = async (request:NextApiRequest, { params }: {params: {[id:string]:string}} ) => {
+export const GET = async (request:Request | NextRequest, { params }: {params: {[id:string]:string}} ) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({creator: params.id}).populate("creator");
